69cdc817582de60cf3875b42ae01bb5a5ad0d092
Hephestsus <aidar.mamytbek2018@gmail.com>
2026-04-08 11:11:21 +0600

    Update MealCard.jsx

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { addToCart } from "../services/cart";
+// import { addToCart } from "../services/cart";
 
 export default function MealCard({ meal, addToCart }) {
   const navigate = useNavigate();

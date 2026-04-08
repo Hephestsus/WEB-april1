1876a6e74306b38a9fe826955e00ede7b8758792
Hephestsus <aidar.mamytbek2018@gmail.com>
2026-04-08 10:44:52 +0600

    Update MealCard.jsx

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,12 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { addToCart } from "../services/cart";
 
 export default function MealCard({ meal }) {
   const navigate = useNavigate();
 
+  const handleAdd = (e) => {
+    e.stopPropagation(); // ❗ чтобы не сработал переход
+    addToCart(meal);
+  };
+
   return (
-    <div className="meal-card" onClick={() => navigate(`/recipe/${meal.idMeal}`)}>
+    <div
+      className="meal-card"
+      onClick={() => navigate(`/recipe/${meal.idMeal}`)}
+    >
       <img src={meal.strMealThumb} />
       <h3>{meal.strMeal}</h3>
+
+      <button onClick={handleAdd}>
+        В корзину
+      </button>
     </div>
   );
 }
